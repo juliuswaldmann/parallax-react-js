b5aefe65f81091c33a08d9fe9c4480603c515314
Julius Waldmann <52603208+juliuswaldmann@users.noreply.github.com>
2021-04-12 13:13:14 +0200

    Delete build directory

--- build/lib/ParallaxLayer.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare const ParallaxLayer: (props: {
-    speed?: string;
-    zIndex?: number;
-    children?: [HTMLElement];
-}) => JSX.Element;
-export default ParallaxLayer;
