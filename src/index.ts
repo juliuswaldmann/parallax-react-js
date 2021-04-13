b5db38f3af800e69a9cf4c3a702a1fe6f0b046ed
juliuswaldmann <julius.waldmann@icloud.com>
2021-04-13 21:25:18 +0200

    sorry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+//Dear future me
+//please forgive me for writing this code
+//I cannot express how sorry I am
+
 import ParallaxContainer from './lib/ParallaxContainer';
 import ParallaxLayer from './lib/ParallaxLayer';
 
